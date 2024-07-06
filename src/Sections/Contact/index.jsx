646a9991974f37400620d8237b9bc358528085ba
Vikas Singh Mann <vickymann9774@gmail.com>
2024-07-05 22:14:59 -0600

    change scroll-to-top-btn position.

--- src/Sections/Contact/index.jsx
+++ src/Sections/Contact/index.jsx
@@ -22,12 +22,12 @@ function Contact() {
                     </div>
                 </div>
             </div>    
-                <ScrollLink to="home" spy={true} smooth={true} duration={10} activeClass="active" className="absolute bottom-32 left-[50%] translate-x-[-50%]">                    
+                <ScrollLink to="home" spy={true} smooth={true} duration={10} activeClass="active" className="absolute bottom-12 lg:bottom-32 left-[50%] translate-x-[-50%]">                    
                 <Button className="bg-green rounded-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevrons-up">
                     <path d="m17 11-5-5-5 5"/><path d="m17 18-5-5-5 5"/></svg></Button>
                 </ScrollLink>
-            <div className="flex justify-center gap-6 mx-auto lg:hidden">          
+            <div className="flex justify-center gap-6 mx-auto lg:hidden mt-10">       
                     <A target="_blank" href="https://github.com/vckymann">
                     <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-github stroke-lightest_slate hover:stroke-green"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg></A>
                     <A target="_blank" href="https://www.instagram.com/vckymann/">
